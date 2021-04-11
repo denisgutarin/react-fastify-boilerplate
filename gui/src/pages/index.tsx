d0999d089f0ebf1aa5c7e5264f295b23296cc985
Denis Gutarin <denis.gutarin@gmail.com>
2021-04-11 22:44:48 +0300

    yarn

--- gui/src/pages/index.tsx
+++ gui/src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React, { ReactElement } from 'react';
 
 export const IndexPage = (): ReactElement => {
+  const Link = (props: Record<string, unknown>): ReactElement => (
+    <a
+      {...props}
+      className="text-indigo-500 hover:text-indigo-700 ease-in-out"
+    />
+  );
   return (
-    <div className="bg-blue-500">
-      Index Page <div>asdasdasdasdas</div>
+    <div className="bg-gray-50 p-10">
+      Index Page{' '}
+      <div>
+        <Link href="#">Link</Link>
+      </div>
     </div>
   );
 };
