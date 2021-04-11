113162656598fe96c090f1d7c150a3519097d5d8
Denis Gutarin <denis.gutarin@gmail.com>
2021-04-11 21:45:13 +0300

    jest

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { IndexPage } from './pages/index';
 
-const App = () => {
+const App = (a: any) => {
   return (
     <Router>
       <Switch>
