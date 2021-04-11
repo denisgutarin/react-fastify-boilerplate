50d1d7f7599e30152c3eb3464046e7f82beebf53
Denis Gutarin <denis.gutarin@gmail.com>
2021-04-11 21:38:09 +0300

    eslint

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { IndexPage } from './pages/index';
 
-type Props = {
-  name?: string;
-};
-
-const App = (props: Props) => {
-  return <h1 className="text-blue-600">Application {props.name}</h1>;
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <IndexPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 export default App;
